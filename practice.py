c9ee2dd79cb967f78553bdd7b3079e19dc28a3cd
Olaiya Olaynka <rachael92.oo@gmail.com>
2023-06-21 00:07:58 +0100

    MySecondCommit

--- practice.py
+++ practice.py
@@ -3,4 +3,7 @@ b=3
 c=a+b
 print(c)
 print("HelloWorld")
-klmlkm;lk;'l/,/.m.jolj;ll;khoihjkyugyudtftyfyu'
+
+Name= input("Enter your name:")
+Address=input("Enter your address")
+print("your name is :", Name , "and address is", Address)
